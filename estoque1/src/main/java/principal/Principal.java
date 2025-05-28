12efcf86ffac955f457d69c2b8fdc8077303b5ae
darkss404 <dark404skull@gmail.com>
2025-05-28 02:16:50 -0300

    adicionar primeiros arquivos

---
+++ estoque1/src/main/java/principal/Principal.java
@@ -0,0 +1,10 @@
+package principal;
+
+import visao.FrmTelaInicial;
+
+public class Principal {
+    public static void main(String[] args) {
+        FrmTelaInicial objetotela = new FrmTelaInicial();
+        objetotela.setVisible(true);
+    }
+}
