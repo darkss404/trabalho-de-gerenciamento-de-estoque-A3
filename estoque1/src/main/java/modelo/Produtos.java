6a2772d0ad734df60f2b0bd2bd72173d5a6d5c5d
Iago Bastos de Oliveira <c4896999809@gmail.com>
2025-05-30 20:11:29 -0300

    Update Produtos.java
    adicionado produtos, categoria, nome, preço, e quantidades mínimas e máximas

--- estoque1/src/main/java/modelo/Produtos.java
+++ estoque1/src/main/java/modelo/Produtos.java
@@ -1,5 +1,101 @@
 package modelo;
 
 public class Produtos {
-    
+    private String nome;
+    private String codigo;
+    private String categoria;
+    private int qtdAtual;
+    private int qtdMinima;
+    private int qtdMaxima;
+    private double preco;
+
+    public Produtos(String nome, String codigo, String categoria, int qtdAtual, int qtdMinima, int qtdMaxima, double preco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.categoria = categoria;
+        this.qtdAtual = qtdAtual;
+        this.qtdMinima = qtdMinima;
+        this.qtdMaxima = qtdMaxima;
+        this.preco = preco;
+    }
+
+    public void entrada(int quantidade) {
+        qtdAtual += quantidade;
+        if (qtdAtual > qtdMaxima) {
+            System.out.println("Estoque de " + nome + " excedeu o máximo (" + qtdAtual + " > " + qtdMaxima + ")");
+        }
+    }
+
+    public void saida(int quantidade) {
+        if (quantidade > qtdAtual) {
+            System.out.println("Estoque insuficiente. Disponível: " + qtdAtual);
+        } else {
+            qtdAtual -= quantidade;
+            if (qtdAtual < qtdMinima) {
+                System.out.println("Estoque de " + nome + " abaixo do mínimo (" + qtdAtual + " < " + qtdMinima + ")");
+            }
+        }
+    }
+
+    public void reajustarPreco(double percentual) {
+        preco *= (1 + percentual / 100);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getQtdAtual() {
+        return qtdAtual;
+    }
+
+    public int getQtdMinima() {
+        return qtdMinima;
+    }
+
+    public int getQtdMaxima() {
+        return qtdMaxima;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setQtdAtual(int qtdAtual) {
+        this.qtdAtual = qtdAtual;
+    }
+
+    public void setQtdMinima(int qtdMinima) {
+        this.qtdMinima = qtdMinima;
+    }
+
+    public void setQtdMaxima(int qtdMaxima) {
+        this.qtdMaxima = qtdMaxima;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+   
+    }
 }
+
