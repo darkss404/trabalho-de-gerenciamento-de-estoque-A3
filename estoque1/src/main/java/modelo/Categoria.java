7e04388ba8047db45d05654a3d30014b47476c2f
hdkil <hdkil@DESKTOP-N5KISQF>
2025-05-31 01:21:45 -0300

    Modelo Categoria

---
+++ estoque1/src/main/java/modelo/Categoria.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Categoria {
+
+    private String nome;
+    private String tamanho;
+    private String embalagem;
+
+    public Categoria(String nome, String tamanho, String embalagem) {
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.embalagem = embalagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public String getEmbalagem() {
+        return embalagem;
+    }
+      public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public void setEmbalagem(String embalagem) {
+        this.embalagem = embalagem;
+    }
+}
