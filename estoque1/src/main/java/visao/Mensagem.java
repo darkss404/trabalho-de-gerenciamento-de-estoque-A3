003496ddcbb57322c23d9f99315007599767f041
hdkil <hdkil@DESKTOP-N5KISQF>
2025-05-31 01:20:13 -0300

    Editando visao

---
+++ estoque1/src/main/java/visao/Mensagem.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package visao;
+
+/**
+ *
+ * @author hdkil
+ */
+public class Mensagem extends Exception {
+
+    /**
+     * Construtor com parâmetro.
+     *
+     * @param mensagem Uma string com a mensagem.
+     */
+    public Mensagem(String mensagem) {
+        super(mensagem);
+    }
+}
